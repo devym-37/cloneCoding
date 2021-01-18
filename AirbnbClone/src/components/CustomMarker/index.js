2677fd70511b000920c4710a1e2501b4a4c7f6a4
Dev-miniyoung <miniyoung37@gmail.com>
2021-01-19 00:02:23 +0900

    google maps marker

---
+++ AirbnbClone/src/components/CustomMarker/index.js
@@ -0,0 +1,41 @@
+import React, { useMemo } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { Marker } from "react-native-maps";
+
+const CustomMarker = (props) => {
+  const { coordinate, price, onPress, isSelected } = props;
+  console.log("isSelected", isSelected);
+
+  const markerSelectedStyle = useMemo(() => {
+    if (isSelected) return [styles.container, { backgroundColor: "black" }];
+    return styles.container;
+  }, [isSelected]);
+
+  const textSelectedStyle = useMemo(() => {
+    if (isSelected) return [styles.text, { color: "white" }];
+    return styles.text;
+  }, [isSelected]);
+
+  return (
+    <Marker coordinate={coordinate} onPress={onPress}>
+      <View style={markerSelectedStyle}>
+        <Text style={textSelectedStyle}>${price}</Text>
+      </View>
+    </Marker>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    padding: 5,
+    borderRadius: 20,
+    borderColor: "grey",
+    borderWidth: 1,
+  },
+  text: {
+    fontWeight: "bold",
+  },
+});
+
+export default CustomMarker;
--- AirbnbClone/src/screens/SearchResultsMap/index.js
+++ AirbnbClone/src/screens/SearchResultsMap/index.js
@@ -1,21 +1,50 @@
-import React from "react";
-import { View, FlatList } from "react-native";
-import MapView from "react-native-maps";
+import React, { useState } from "react";
+import { View, StyleSheet } from "react-native";
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
+import CustomMarker from "../../components/CustomMarker";
+import places from "../../../assets/data/feed";
 
 const SearchResultsMap = () => {
+  const [selectedPlaceId, setSelectedPlaceId] = useState(null);
+
   return (
-    <View style={{ width: "100%", height: "100%" }}>
+    <View style={styles.container}>
       <MapView
-        style={{ width: "100%", height: "100%" }}
+        style={styles.mapContainer}
+        provider={PROVIDER_GOOGLE}
         initialRegion={{
           latitude: 28.3279822,
           longitude: -16.5124847,
           latitudeDelta: 0.8,
           longitudeDelta: 0.8,
         }}
-      />
+      >
+        {places.map((place, index) => {
+          const { coordinate, totalPrice, newPrice, id } = place;
+          return (
+            <CustomMarker
+              coordinate={coordinate}
+              price={newPrice}
+              isSelected={id === selectedPlaceId}
+              onPress={() => setSelectedPlaceId(id)}
+              key={index.toString()}
+            />
+          );
+        })}
+      </MapView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: "100%",
+  },
+  mapContainer: {
+    width: "100%",
+    height: "100%",
+  },
+});
 
 export default SearchResultsMap;
