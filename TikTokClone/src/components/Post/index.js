5b3e356b2d027965e206f825f4a29423f65b863c
Dev-miniyoung <miniyoung37@gmail.com>
2021-03-17 23:56:37 +0900

    화면터치시 재생정지

--- TikTokClone/src/components/Post/index.js
+++ TikTokClone/src/components/Post/index.js
@@ -1,20 +1,36 @@
-import React from 'react';
-import { View, Text, StyleSheet, Dimensions } from 'react-native';
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  TouchableWithoutFeedback,
+} from 'react-native';
 
 import Video from 'react-native-video';
 
 const Post = () => {
+  const [paused, setPaused] = useState(false);
+
+  const onPlayPausePress = () => {
+    setPaused(!paused);
+  };
+
   return (
     <View style={styles.container}>
-      <Video
-        source={{
-          uri:
-            'https://d8vywknz0hvjw.cloudfront.net/fitenium-media-prod/videos/45fee890-a74f-11ea-8725-311975ea9616/proccessed_720.mp4',
-        }}
-        style={styles.video}
-        resizeMode={'cover'}
-        onError={(e) => console.log('error', e)}
-      />
+      <TouchableWithoutFeedback onPress={onPlayPausePress}>
+        <Video
+          source={{
+            uri:
+              'https://d8vywknz0hvjw.cloudfront.net/fitenium-media-prod/videos/45fee890-a74f-11ea-8725-311975ea9616/proccessed_720.mp4',
+          }}
+          style={styles.video}
+          resizeMode={'cover'}
+          onError={(e) => console.log('error', e)}
+          repeat={true}
+          paused={paused}
+        />
+      </TouchableWithoutFeedback>
     </View>
   );
 };
